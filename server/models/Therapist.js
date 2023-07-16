2ff455007ed4c1a6b017e5ee46c7c7071e0c7344
Joseph Flatley <josephflatley@Josephs-MacBook-Pro.local>
2023-07-15 22:51:02 -0500

    Therapist models

---
+++ server/models/Therapist.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const therapistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    specialty: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+});
+
+const Therapist = mongoose.model('Therapist', therapistSchema);
+
+module.exports = Therapist;
