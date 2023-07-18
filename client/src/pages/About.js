9718964a650ff17ac2fa29e7cb376476904cb51d
Stephanie Jauch <sjauch21@gmail.com>
2023-07-17 21:03:33 -0500

    const data

---
+++ client/src/pages/About.js
@@ -0,0 +1,20 @@
+//import styling
+//import images from img folder
+
+const data = [
+    {
+        heading: 'Break out of the cycle',
+        text: "Seeking mental health help shows tremendous strength and a genuine commitment to personal growth and longterm well-being",
+        image: behindBars,
+    },
+    {
+        heading: 'Find your mental health professional',
+        text: "Choose from top therapists who have experience in helping people just like you embrace the healing they deserve",
+        image: twoPeople,
+    },
+    {
+        heading: 'Continue investing in a brighter tomorrow',
+        text: "Leave comments on therapist's profiles for other's on their journey to help them find the professional that best suits them"
+        image: notePad,
+    }
+]
