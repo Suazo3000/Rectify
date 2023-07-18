14e766872dcd9f7a83047780cea721b8b0d8717a
Stephanie Jauch <sjauch21@gmail.com>
2023-07-17 21:15:09 -0500

    function

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,9 +4,9 @@
 const data = [
     {
         heading: 'Our Mission',
-        text: "Our mission is to empower former inmates with the necessary counseling and support to facilitate their successful reintegration into society, fostering personal growth and enabling them to become positive contributors to their communities. Through comprehensive and tailored counseling programs, we strive to break the cycle of recidivism, promoting individual transformation and societal well-being."
-        image: helpingHands
-    }
+        text: "Our mission is to empower former inmates with the necessary counseling and support to facilitate their successful reintegration into society, fostering personal growth and enabling them to become positive contributors to their communities. Through comprehensive and tailored counseling programs, we strive to break the cycle of recidivism, promoting individual transformation and societal well-being.",
+        image: helpingHands,
+    },
     {
         heading: 'Break out of the cycle',
         text: "Seeking mental health help shows tremendous strength and a genuine commitment to personal growth and longterm well-being",
@@ -21,5 +21,24 @@ const data = [
         heading: 'Continue investing in a brighter tomorrow',
         text: "Help others rewrite their story to live a life filled with purpose, joy and fulfillment"
         image: notePad,
-    }
-]
+    },
+];
+
+const About = () => {
+    return (
+        <main className="bodyContent" id="aboutContent">
+            {data.map((item, i) =>{
+                return (
+                    <AboutSection
+                    key={i}
+                    sections={i}
+                    heading={item.heading}
+                    text={item.text}
+                    image={item.image}
+                    />
+                );
+            })}
+        </main>
+    );
+};
+
