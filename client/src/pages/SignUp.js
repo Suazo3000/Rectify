b9094f86ba4b16ef81341b2b0fa2045b302c7c02
Suazo3000 <suazosteve1@gmail.com>
2023-07-23 06:28:59 -0500

    added required modules and dependencies

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -6,6 +6,22 @@ import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme();
+
 const SignUp = () => {
   const [formState, setFormState] = useState({
     username: '',
